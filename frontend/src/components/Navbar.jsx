0d1e47074b5c2c41a7dc15a5c9a222afd411b65b
Aman Kumar <amansinha327@gmail.com>
2025-11-17 07:50:17 +0000

    Updated Logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import "./Navbar.css";
+import logo from "../assets/logo.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
+      {/* Left side: logo + brand text */}
       <div className="nav-left">
-        <div className="logo-circle">🧠</div>
+        <img src={logo} alt="Logo" className="site-logo" />
         <span className="logo-text">Reddit Ideas</span>
       </div>
 
+      {/* Right side: GitHub button */}
       <div className="nav-right">
         <a
           href="https://github.com/Siphonite/reddit_ideas"
